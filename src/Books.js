665697429b09c954e47ee36d127380f3143adb57
laurenlucero <lucerolaurenm@gmail.com>
2021-07-29 11:24:40 -0400

    Initial commit

---
+++ src/Books.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+
+const BOOKS = gql`
+  query GetBooks {
+    books {
+      title
+      author
+    }
+  }
+`
+
+const Books = () => {
+  
+  const { loading, error, data } = useQuery(BOOKS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+
+  return data.books.map(({ title, author }) => (
+      <div key={title}>
+        <p>{title} by {author}</p>
+      </div>
+    ));
+  }
+ 
+export default Books;
